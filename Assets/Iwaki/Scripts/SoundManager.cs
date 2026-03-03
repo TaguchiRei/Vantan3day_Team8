8b62e2d24cfe8ef3b2fb31faeac3d8c99b7eff96
IwakiEisuke <get10.ref11.del12@gmail.com>
2026-03-03 11:51:57 +0900

    [add] サウンドの再生を管理するクラスを追加

---
+++ Assets/Iwaki/Scripts/SoundManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private BGMDatabase _bgmDatabase;
+    [SerializeField] private SEDatabase _seDatabase;
+
+    [SerializeField] private AudioSource _bgmAudioSource;
+    [SerializeField] private AudioSource _seAudioSource;
+    
+    private static SoundManager Instance
+    {
+        get
+        {
+            if (_instance != null) return _instance;
+            
+            _instance = FindAnyObjectByType<SoundManager>();
+            
+            if (_instance == null)
+            {
+                Debug.LogError("SoundManager が見つかりません");
+            }
+            
+            return _instance;
+        }
+    }
+    
+    private static SoundManager _instance;
+    
+    
+    public static void PlayBGM(BGMType type)
+    {
+        Instance.Play(type);
+    }
+
+    public static void PlaySE(SEType type)
+    {
+        Instance.Play(type);
+    }
+    
+    private void Play(BGMType type)
+    {
+        if (_bgmDatabase == null)
+        {
+            Debug.LogError("[SoundManager] BGM Databaseが存在しません", this);
+            return;
+        }
+        
+        var bgm = _bgmDatabase.GetBGM(type);
+        _bgmAudioSource.clip = bgm;
+        _bgmAudioSource.Play();
+    }
+
+    private void Play(SEType type)
+    {
+        if (_seDatabase == null)
+        {
+            Debug.LogError("[SoundManager] SE Databaseが存在しません", this);
+            return;
+        }
+        
+        var se = _seDatabase.GetSE(type);
+        _seAudioSource.PlayOneShot(se);
+    }
+}
