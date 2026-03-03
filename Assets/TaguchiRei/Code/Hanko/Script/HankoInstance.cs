c291a9dc8f7947271657257957a2789f7fce1ce7
AoiTomiyama <TadoO.o2209@gmail.com>
2026-03-03 11:44:49 +0900

    [add]アニメーション作成

---
+++ Assets/TaguchiRei/Code/Hanko/Script/HankoInstance.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HankoInstance : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer _spriteRenderer;
+	[SerializeField] private Animator _animator;
+
+	public void PressTheStamp(Texture2D hankoTexture)
+	{
+
+	}
+}
