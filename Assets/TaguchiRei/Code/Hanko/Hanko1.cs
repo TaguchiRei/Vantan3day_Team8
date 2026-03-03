af13513ce6bfe3db7d9763f60dc9f57e8b567f83
reitaghchi <ren.len2426@gmail.com>
2026-03-03 10:33:23 +0900

    [add]ハンコデータ用ファイル作成
    ・PCのバッテリーを忘れたので富山PCで作業を続行

---
+++ Assets/TaguchiRei/Code/Hanko/Hanko1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Hanko", menuName = "Scriptable Objects/Hanko1")]
+public class Hanko1 : ScriptableObject
+{
+    
+}
+
+public enum HankoType
+{
+    
+}
