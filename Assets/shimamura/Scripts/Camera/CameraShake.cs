4454ca1a41099027276104965eb1221ee3044a4d
shimamura <shimakou779@gmail.com>
2026-03-05 12:38:21 +0900

    [update]shakeのGainをインスペクターで変更できるようにした

--- Assets/shimamura/Scripts/Camera/CameraShake.cs
+++ Assets/shimamura/Scripts/Camera/CameraShake.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
     [SerializeField] private CinemachineImpulseSource _cinemachineImpulseSource;
+    [SerializeField] private float _gain = 0.5f;
 
     public void OnShake()
     {
-        _cinemachineImpulseSource.GenerateImpulse();
+        _cinemachineImpulseSource.GenerateImpulse(_gain);
     }
 }
